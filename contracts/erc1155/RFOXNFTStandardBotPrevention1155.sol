109938387cbe76b8bddaeeded4c2df13f2dc257e
ssinar <santos.sinar@student.umn.ac.id>
2023-02-10 08:57:13 +0700

    L01: lock pragma version

--- contracts/erc1155/RFOXNFTStandardBotPrevention1155.sol
+++ contracts/erc1155/RFOXNFTStandardBotPrevention1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "./lib/RFOXNFTSignatureSale1155.sol";
 import "./structs/ParamStructs1155.sol";
--- contracts/mock/MockContractBuyer.sol
+++ contracts/mock/MockContractBuyer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 interface IRFOXNFT {
   function buyNFTsPresale(uint256 tokensNumber, bytes32[] calldata proof) external;
